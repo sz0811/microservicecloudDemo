add0b3a67804b04a73c16217ac2144fb86d2e0da
孙志 <sunzhi@sunzhideMacBook-Pro.local>
2020-08-13 18:36:23 +0800

    microservice-api/

---
+++ microservice-api/src/main/java/com/atguigu/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Dept;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService> {
+    public DeptClientService create(Throwable throwable) {
+        return new DeptClientService() {
+            public Dept get(Long id) {
+                return new Dept().setDeptno(id).setDname("该id"+id+"没有对应的信息，Consumer客户端提供的降级信息，此刻服务Provider已经关闭").setDb_source("no this database in mysql");
+            }
+
+            public List<Dept> list() {
+                return null;
+            }
+
+            public boolean add(Dept dept) {
+                return false;
+            }
+        };
+    }
+}
